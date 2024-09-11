20c4752d521909ab40879e6b935484e03585f6ac
aliyassine101 <aliyasine901@gmail.com>
2024-09-11 16:50:13 +0300

    Delete dist directory

--- dist/NodejsExpressTypescriptProjectStarter-Libanux-master/constants/routesConstants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ROUTES = void 0;
-// src/constants/routesConstants.ts
-exports.ROUTES = {
-    SIGNUP: '/SIGNUP',
-    SIGNIN: '/SIGNIN'
-};
